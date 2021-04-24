d72e03044202d20b8be527be62e93632b77c68ec
Aleksey1103 <47826423+Aleksey1103@users.noreply.github.com>
2021-04-24 17:55:11 +0300

    1-module 4-task refactoring

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,5 +1,5 @@
 function checkSpam(str) {
-  let lowerCaseStr = str.toLowerCase();
+  const lowerCaseStr = str.toLowerCase();
 
   return lowerCaseStr.includes('1xbet') || lowerCaseStr.includes('xxx');
 }
