421b54c12c3685f23193d6cf3207598261e94d0f
Aleksey1103 <47826423+Aleksey1103@users.noreply.github.com>
2021-04-24 11:52:13 +0300

    1-module 3-task refactoring

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,7 @@
 function ucFirst(str) {
-  return (!str) ? str : (!str[1]) ? str.toUpperCase() : str[0].toUpperCase() + str.slice(1);
+  if (!str) {
+    return str;
+  }
+
+  return (!str[1]) ? str.toUpperCase() : str[0].toUpperCase() + str.slice(1);
 }
