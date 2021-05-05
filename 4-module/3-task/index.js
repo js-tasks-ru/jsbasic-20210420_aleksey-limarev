12646aedca6c810c35831fff47d0babffbc14718
Aleksey1103 <47826423+Aleksey1103@users.noreply.github.com>
2021-05-05 09:16:56 +0300

    make function 'highlight' to change table styles

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
-  // ваш код...
+  if (!table) {return;}
+
+  [...table.tBodies].reduce((a, b) => a.concat([...b.rows]), [])
+                    .forEach(tr => {
+                      const [status, gender, age] = [...tr.children].reverse();
+
+                      switch (status.dataset.available) {
+                      case 'true':
+                        tr.classList.add('available');
+                        break;
+                      case 'false':
+                        tr.classList.add('unavailable');
+                        break;
+                      case undefined:
+                        tr.hidden = true;
+                        break;
+                      }
+
+                      switch (gender.textContent) {
+                      case "m":
+                        tr.classList.add('male');
+                        break;
+                      case "f":
+                        tr.classList.add('female');
+                        break;
+                      }
+
+                      tr.style.textDecoration = Number(age.textContent) < 18 ? 'line-through' : '';
+                    });
 }
