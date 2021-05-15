e22f97f3e9a007e71b8c18893599e0047ac08a37
Aleksey1103 <47826423+Aleksey1103@users.noreply.github.com>
2021-05-15 16:22:32 +0300

    make Class for user table

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,60 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this._table = document.createElement("table");
+    this._users = rows;
+    this._titles = ["Имя", "Возраст", "Зарплата", "Город", ""];
+
+    this._renderTable();
+  }
+
+  _tableCellTemplate({ type, value }) {
+    return `<${type}>${value}</${type}>`;
+  }
+
+  _onBtnCloseClick(event) {
+    if (event.target.tagName !== "BUTTON") {return;}
+
+    /*
+    намеренно не использовал стрелочную функцию, так как при обработке клика
+    в качестве this будет использован DOM элемент tr, которому навешен этот
+    обработчик и который собственно и нужно удалить
+    */
+    this.remove();
+  }
+
+  _tableBodyRowTemplate({name, age, salary, city}) {
+    const cellValues = [name, age, salary, city, "<button>X</button>"];
+
+    return `<tr data-type="content">
+                ${ cellValues.map((field) => this._tableCellTemplate({type: "td", value: field})).join('') }
+            </tr>`;
+  }
+
+  _tableBodyTemplate() {
+    return `<tbody>
+                ${ this._users.map((user) => this._tableBodyRowTemplate(user)).join('') }
+            </tbody>`;
+  }
+
+  _tableHeadTemplate() {
+    return `<thead>
+              <tr>
+                ${ this._titles.map((title) => this._tableCellTemplate({type: "th", value: title})).join('') }
+              </tr>
+            </thead>`;
+  }
+
+  _renderTable() {
+    this._table.innerHTML += this._tableHeadTemplate();
+    this._table.innerHTML += this._tableBodyTemplate();
+
+    this._table.querySelectorAll("[data-type='content']").forEach(tr => {
+      tr.addEventListener("click", this._onBtnCloseClick);
+    });
+  }
+
+  get elem() {
+    return this._table;
   }
 }
